00b066e473500e48b52cad331e981cf0203904f7
Abhishek Krishnan Rathaur <102174500+abhishekrathaur2004@users.noreply.github.com>
2024-06-06 17:26:03 +0530

    JS class quick revision

---
+++ class.js
@@ -0,0 +1,17 @@
+// It is a template which is used for creating object
+
+class Square{
+    constructor(){
+        // methods which is used for asigning the properties at the time of object creation
+
+        this.side = 10;  // default value for eevery square side
+
+    }
+}
+
+let sq1 = new Square();
+// console.log(sq1);
+// Square { side: 10 }
+let sq2 = new Square();
+// console.log('sq2', sq2);
+// sq2 Square { side: 10 }
